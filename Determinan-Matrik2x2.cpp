67c1def2e75b54d22b4773b84a74ac893989104d
alb-soul <100475822+alb-soul@users.noreply.github.com>
2022-03-23 16:30:45 +0700

    Kode program C++

--- Determinan-Matrik2x2.cpp
+++ Determinan-Matrik2x2.cpp
@@ -1,12 +1,20 @@
 #include <iostream>
+#include <limits>
 int main() {
     int matrik[2][2];
     //Pengisian nilai matrik
     for (int j=0;j<2;j++) {
         std::cout << "Isi nilai matriks baris " << j+1 << " kolom 1-2" << std::endl;
         for (int i=0;i<2;i++) {
+ulang:    
             std::cout << "Kolom ke-" << i+1 << " : ";
-            std::cin >> matrik[j][i];
+            std::cin >> matrik[j][i]; 
+            if (std::cin.fail()) {
+                std::cin.clear();
+                std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+                std::cout << "\nHanya masukkan nilai integer!" << std::endl;
+                goto ulang;
+            }
         }
     }
     //Penghitungan determinan
